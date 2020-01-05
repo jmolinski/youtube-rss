25ce20f76e59bfb5145e8a7fe3d70561f272a1d7
Jakub Molinski <jakub@molinski.dev>
2020-01-05 23:40:47 +0100

    Add combined feed (mainpage + yt)

--- backend/platforma/platforma/views.py
+++ backend/platforma/platforma/views.py
@@ -7,6 +7,10 @@ import pytz
 from django.http.response import HttpResponse
 import os
 import json
+import atoma
+import requests
+import xml.etree.ElementTree as ET
+from email.utils import parsedate_to_datetime
 
 
 def download_video(id):
@@ -79,12 +83,12 @@ def update_local(request):
     return HttpResponse("OK")
 
 
-def create_nr_podcast():
+def create_nr_podcast(extra):
     p = Podcast()
-    p.name = "Nocne Radio YouTube"
+    p.name = extra["name"]
     p.description = "Feed kanalu youtube nocnego radia"
     p.language = "pl"
-    p.feed_url = settings.NR_FEED_DOMAIN + "feeds/nr/feed/"
+    p.feed_url = settings.NR_FEED_DOMAIN + "feeds/nr/feed/" + extra["url"]
     p.explicit = False
     p.complete = False
     p.new_feed_url = settings.NR_FEED_DOMAIN + "feeds/nr/feed/"
@@ -98,23 +102,36 @@ def add_episode(feed, video_data):
     e1 = feed.add_episode()
     e1.id = video_data["id"]
     e1.title = video_data["title"]
-    e1.summary = htmlencode(video_data["description"])
+    if "parsed_description" in video_data:
+        e1.summary = video_data["parsed_description"]
+    else:
+        e1.summary = htmlencode(video_data["description"])
     e1.image = video_data["thumbnail"]
-    e1.publication_date = datetime.datetime(
-        video_data["year"],
-        video_data["month"],
-        video_data["day"],
-        12,
-        0,
-        0,
-        tzinfo=pytz.utc,
-    )
-    e1.media = Media(
-        video_data["media_url"],
-        video_data["size"],
-        type="audio/" + video_data["extension"],
-        duration=datetime.timedelta(seconds=video_data["duration"]),
-    )
+    if "exact_date" in video_data:
+        e1.publication_date = video_data["exact_date"]
+    else:
+        e1.publication_date = datetime.datetime(
+            video_data["year"],
+            video_data["month"],
+            video_data["day"],
+            12,
+            0,
+            0,
+            tzinfo=pytz.utc,
+        )
+    if "duration" in video_data:
+        e1.media = Media(
+            video_data["media_url"],
+            video_data["size"],
+            type="audio/" + video_data["extension"],
+            duration=datetime.timedelta(seconds=video_data["duration"]),
+        )
+    else:
+        e1.media = Media(
+            video_data["media_url"],
+            video_data["size"],
+            type="audio/" + video_data["extension"],
+        )
 
 
 def get_saved_ids():
@@ -161,11 +178,62 @@ def get_file_data(filename):
     }
 
 
+def parse_raw_nr_item(item):
+    enclosure = item.find("enclosure")
+    media_url = enclosure.get("url")
+    filesize = int(enclosure.get("length"))
+    date = item.find("pubDate").text
+
+    return {
+        "media_url": media_url,
+        "size": filesize,
+        "id": media_url,
+        "title": item.find("title").text,
+        "url": media_url,
+        "filesize_raw": filesize,
+        "parsed_description": item.find("description").text,
+        "thumbnail": item.find("{http://www.itunes.com/dtds/podcast-1.0.dtd}image").get(
+            "href"
+        ),
+        "extension": "mp3",
+        "sortby": 0,
+        "exact_date": parsedate_to_datetime(date),
+    }
+
+
+def get_radio_feed():
+    try:
+        data = requests.get("http://nocneradio.pl/feed/")
+        root = ET.fromstring(data.content.decode("utf-8"))
+
+        raw_items = [x for x in root.find("channel").getchildren() if x.tag == "item"]
+        for item in raw_items:
+            try:
+                yield parse_raw_nr_item(item)
+            except Exception as e:
+                print(e)
+    except:
+        pass
+
+
 def get_rss_feed(request):
-    nr = create_nr_podcast()
+    nr = create_nr_podcast({"url": "", "name": "Nocne Radio YouTube Channel"})
     ids = list(get_saved_ids())  # 'id.ext'
 
     for ep in ids:
         add_episode(nr, get_file_data(ep))
+
+    return HttpResponse(nr.rss_str(), content_type="text/xml")
+
+
+def get_combined_rss_feed(request):
+    nr = create_nr_podcast({"url": "combined/", "name": "Nocne Radio (+ YouTube)"})
+    ids = list(get_saved_ids())  # 'id.ext'
+
+    for ep in ids:
+        add_episode(nr, get_file_data(ep))
+
+    for ep in get_radio_feed():
+        add_episode(nr, ep)
 
     return HttpResponse(nr.rss_str(), content_type="text/xml")
