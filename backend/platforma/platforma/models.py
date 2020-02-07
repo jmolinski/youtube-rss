8f968a77afdf6f8ca206a3b2648fce2d2ca4805a
Jakub Molinski <jakub@molinski.dev>
2020-02-07 18:28:18 +0100

    Wait 8h before downloading video

--- backend/platforma/platforma/models.py
+++ backend/platforma/platforma/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.conf import settings
 from django.db import models
 
 
@@ -7,14 +10,24 @@ class Episode(models.Model):
     hidden = models.BooleanField(default=False)
     draft_posted = models.BooleanField(default=False)
     deleted_old = models.BooleanField(default=False)
-    download_date = models.DateTimeField(null=True, default=None)
+    first_seen = models.DateTimeField(null=True, auto_now_add=True)
     redownloaded = models.BooleanField(default=False)
     remote_filename = models.CharField(max_length=1000, null=True, blank=True)
+    file_downloaded = models.BooleanField(default=False)
 
     def is_visible(self):
-        if self.deleted_old or self.hidden or self.currently_downloading:
-            return False
-        return True
+        return (
+            not (self.deleted_old or self.hidden or self.currently_downloading)
+            and self.file_downloaded
+        )
 
     def get_filename(self):
         return f"{self.youtube_id}.mp3"
+
+    def should_download(self):
+        # returns true if the file is at least X hours old
+        tdelta = datetime.datetime.now() - self.first_seen
+        enough_time_elapsed = int(tdelta.total_seconds()) > (
+            settings.MIN_VIDEO_AGE_H * 60 * 60
+        )
+        return not self.file_downloaded and enough_time_elapsed
--- backend/platforma/platforma/update_local.py
+++ backend/platforma/platforma/update_local.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import os
+import random
 import subprocess
 
 from django.conf import settings
@@ -8,52 +9,62 @@ from platforma.platforma import models
 from platforma.platforma.services import get_file_data
 
 
-def download_video(id):
-    if models.Episode.objects.filter(
-        youtube_id=id, currently_downloading=True
-    ).exists():
+def download_video_call_ytdl(video_id: str):
+    ret = subprocess.call(
+        [
+            "youtube-dl",
+            "-f",
+            "bestaudio",
+            "--extract-audio",
+            "--audio-format",
+            "mp3",
+            "--audio-quality",
+            "96K",
+            "--write-info-json",
+            "--output",
+            r"/app/shared/media/%(id)s.download.temp",
+            f"https://www.youtube.com/watch?v={video_id}",
+        ],
+        shell=False,
+    )
+    subprocess.call(
+        [
+            "mv",
+            f"/app/shared/media/{video_id}.download.temp.info.json",
+            f"/app/shared/media/{video_id}.info.json",
+        ],
+        shell=False,
+    )
+    subprocess.call(
+        [
+            "mv",
+            f"/app/shared/media/{video_id}.download.mp3",
+            f"/app/shared/media/{video_id}.mp3",
+        ],
+        shell=False,
+    )
+
+    return ret
+
+
+def download_video(episode: models.Episode):
+    episode.refresh_from_db()
+    id = episode.youtube_id
+    if episode.currently_downloading:
         print(f"Skipping {id} [the video is already being processed]")
         return -1
-    if models.Episode.objects.filter(youtube_id=id).exists():
-        print(f"Skipping {id} [the video has already been processed or is obsolete]")
+    if episode.is_visible() or not episode.should_download():
+        print(f"Skipping {id} [the video has already been processed or is too young]")
         return -2
 
-    episode = models.Episode(youtube_id=id, currently_downloading=True)
+    episode.currently_downloading = True
     episode.save()
     try:
-        ret = subprocess.call(
-            [
-                "youtube-dl",
-                "-f",
-                "bestaudio",
-                "--extract-audio",
-                "--audio-format",
-                "mp3",
-                "--audio-quality",
-                "96K",
-                "--write-info-json",
-                "--output",
-                r"/app/shared/media/%(id)s.download.temp",
-                f"https://www.youtube.com/watch?v={id}",
-            ],
-            shell=False,
-        )
-        subprocess.call(
-            [
-                "mv",
-                f"/app/shared/media/{id}.download.temp.info.json",
-                f"/app/shared/media/{id}.info.json",
-            ],
-            shell=False,
-        )
-        subprocess.call(
-            [
-                "mv",
-                f"/app/shared/media/{id}.download.mp3",
-                f"/app/shared/media/{id}.mp3",
-            ],
-            shell=False,
-        )
+        ret = download_video_call_ytdl(episode.youtube_id)
+
+        if int(ret) == 0:
+            episode.file_downloaded = True
+            episode.save()
 
         return ret
     except Exception as e:
@@ -64,7 +75,7 @@ def download_video(id):
         episode.save()
 
 
-def update_local():
+def get_new_videos_and_add_to_database():
     get_ids_process = subprocess.run(
         [
             "youtube-dl",
@@ -81,38 +92,18 @@ def update_local():
         i for i in get_ids_process.stdout.decode("utf-8").split("\n") if len(i) > 3
     }
 
-    already_downloaded = {
-        x.youtube_id
-        for x in [y for y in models.Episode.objects.all() if y.is_visible()]
-    }
-    currently_downloading = {
-        x.youtube_id
-        for x in models.Episode.objects.filter(currently_downloading=True).all()
-    }
-    to_skip = ids_to_download & already_downloaded
-    ids_to_download = ids_to_download - (already_downloaded | currently_downloading)
+    for i in ids_to_download:
+        if not models.Episode.objects.filter(youtube_id=i).exists():
+            episode = models.Episode(youtube_id=id)
+            episode.save()
 
-    print("Skipping already downloaded ids:", to_skip)
-    if currently_downloading:
-        print("Skipping currently downloading ids:", currently_downloading)
 
-    if len(ids_to_download) > 5:
-        print("Capping full list to download [", ids_to_download, "] to [", end=" ")
-        ids_to_download = set(list(ids_to_download)[:5])
-        print(ids_to_download, "] to not git 429 too many requests")
-
-    if ids_to_download:
-        pool = multiprocessing.Pool(processes=settings.CONCURRENT_DOWNLOADS)
-        print(pool.map(download_video, list(ids_to_download)))
-    else:
-        print("Nothing to download")
-
-    ######### clear old files
-
+def remove_obsolete_files():
     saved_filenames = [
         y.get_filename() for y in models.Episode.objects.all() if y.is_visible()
     ]
     saved_videos = list(map(get_file_data, saved_filenames))
+
     if len(saved_videos) > settings.REMOVE_THRESHOLD_N:
         saved_videos.sort(key=lambda x: x["sortby"])
         vid = saved_videos[0]  # to_delete
@@ -121,10 +112,43 @@ def update_local():
             "/app/shared/media/", vid["media_url"].split("media/")[1]
         )
         metadata_path = os.path.join("/app/shared/media/", vid["id"] + ".info.json")
-        print("Removing:", vid_path, metadata_path)
+        print("Removing obsolete video:", vid_path, metadata_path)
 
         os.remove(vid_path)
         os.remove(metadata_path)
         episode = models.Episode.objects.get(youtube_id=vid["id"])
         episode.deleted_old = True
         episode.save()
+
+
+def update_local():
+    get_new_videos_and_add_to_database()
+
+    eps_to_download = [
+        e
+        for e in models.Episode.objects.filter(
+            file_downloaded=False, currently_downloading=False, deleted_old=False
+        )
+        if e.should_download()
+    ]
+
+    if len(eps_to_download) > 5:
+        print(
+            "Capping full list to download [",
+            [e.youtube_id for e in eps_to_download],
+            "] to [",
+            end=" ",
+        )
+        eps_to_download = set(random.shuffle(eps_to_download)[:5])
+        print(
+            [e.youtube_id for e in eps_to_download],
+            "] to not get 429 too many requests",
+        )
+
+    if eps_to_download:
+        pool = multiprocessing.Pool(processes=settings.CONCURRENT_DOWNLOADS)
+        print(pool.map(download_video, list(eps_to_download)))
+    else:
+        print("Nothing to download")
+
+    remove_obsolete_files()
